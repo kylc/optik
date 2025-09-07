d53b4eb0759d64c76499c051301ad3642a5ebe26
Kyle Cesare <kcesare@gmail.com>
2025-09-06 19:48:03 -0600

    Clean up flake.nix and add automatic venv.

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
 
-    naersk.url = "github:nix-community/naersk/master";
+    naersk.url = "github:nix-community/naersk";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
@@ -15,38 +15,48 @@
       perSystem = { config, self', pkgs, lib, system, ... }:
         let
           naersk-lib = pkgs.callPackage inputs.naersk { };
-
-          # Use the nightly channel so that we can use the faster cranelift
-          # backend for development builds.
           rustToolchain = pkgs.rust-bin.nightly.latest.default.override {
-            extensions =
-              [ "rust-src" "rust-analyzer" "clippy" "rustc-codegen-cranelift" ];
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+              "clippy"
+              "rustc-codegen-cranelift-preview"
+            ];
           };
-
-          # Some packages are required for building the native dependencies.
-          rustBuildInputs = with pkgs; [
-            patchelf
-            cmake
-            gcc
-            mold
-            openssl
-            pkg-config
-            python3
-          ];
-        in {
+        in rec {
           _module.args.pkgs = import inputs.nixpkgs {
             inherit system;
             overlays = [ inputs.rust-overlay.overlays.default ];
           };
 
-          packages.default = naersk-lib.buildPackage ./.;
+          packages.default = naersk-lib.buildPackage {
+            src = ./.;
+            nativeBuildInputs = with pkgs; [ gcc cmake lld openssl python3 ];
+          };
 
           devShells.default = pkgs.mkShell {
             name = "optik-dev";
-            buildInputs = rustBuildInputs;
-            nativeBuildInputs =
-              [ rustToolchain pkgs.maturin pkgs.python3Packages.numpy ];
-            shellHook = ''
+            nativeBuildInputs = with pkgs;
+              packages.default.nativeBuildInputs ++ [
+                rustToolchain
+                cargo-nextest
+                cargo-outdated
+
+                # For C++ bindings
+                eigen
+
+                # For Python bindings
+                maturin
+                pyright
+                python3Packages.numpy
+                python3Packages.black
+                python3Packages.ruff
+                python3Packages.venvShellHook
+              ];
+
+            # Create a virtualenv for Python development with maturin.
+            venvDir = "./.venv";
+            postShellHook = ''
               # For rust-analyzer 'hover' tooltips to work.
               export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library";
             '';
